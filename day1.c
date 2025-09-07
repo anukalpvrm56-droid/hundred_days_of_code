fa74e45b678b5203c91a3226532c3727f2925d1f
anukalpvrm56-droid <anukalpvrm56@gmail.com>
2025-09-07 21:23:42 +0530

    Initial commit

---
+++ day1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    printf("enter the value of the number 1\n");
+    scanf("%d", &a);
+    
+    printf("enter the value of the second number\n");
+    scanf("%d", &b);
+
+    int sum;
+    sum == a+b;
+    
+    printf("the sum of the two number is %d", sum);
+
+
+
+}
