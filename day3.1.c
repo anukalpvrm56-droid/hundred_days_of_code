50893a7276bdefd4be19b02c7d0ed74c1ab9491f
anukalpvrm56-droid <anukalpvrm56@gmail.com>
2025-09-07 22:28:42 +0530

    ADD day3.1.c

---
+++ day3.1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    float cel;
+    float fer;
+    
+    printf("enter the value of the celcious ")
+    scanf("%f\n", &cel );
+    
+    fer = (cel*1.8)+32;
+
+    printf("the value in the ferhanite is %f", &fer);
+
+
+
+    return 0;
+}
