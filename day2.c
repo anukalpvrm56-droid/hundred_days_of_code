db070f00aaf8e60e10fcc3aa2fdba89a1f9f61d7
anukalpvrm56-droid <anukalpvrm56@gmail.com>
2025-09-07 21:30:47 +0530

    Initial commit

---
+++ day2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(){
+    int a,b;
+    printf("enter the value of the number 1\n");
+    scanf("%d", &a);
+    
+    printf("enter the value of the second number\n");
+    scanf("%d", &b);
+
+    int sum;
+    sum == a+b;
+
+
+    int diff;
+    diff == a-b;
+
+
+    int mul;
+    mul == a*b;
+
+
+    int ques;
+    ques == a+b;
+    
+    printf("the sum of the two number is %d", sum);
+    printf("the sum of the two number is %d", diff);
+    printf("the sum of the two number is %d", mul);
+    printf("the sum of the two number is %d", ques);
+
+}
