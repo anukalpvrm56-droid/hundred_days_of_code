2e0061dbc81ffd12eb592837928e002dc36ef465
anukalpvrm56-droid <anukalpvrm56@gmail.com>
2025-09-07 22:58:17 +0530

    ADD day3.c

---
+++ day3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(){
+int a;
+int b; 
+int c;
+
+printf("enter the value of the 1 number");
+scanf("%d\n", &a);
+
+printf("enter the value of the 2 number");
+scanf("%d\n", &b);
+
+a = c;
+a = b;
+b = c;
+
+printf("the new value of the 1 number is %d", a);
+printf("the new value of the 2 number is %d ", b);
+
+
+
+    return 0;
+}
