241679a6efa8044a5b1adc3424c2595553aa5d6f
anukalpvrm56-droid <anukalpvrm56@gmail.com>
2025-09-09 17:31:46 +0530

    this is the code for 10 days

---
+++ day10.1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    int a, b, c;
+    printf("enter the sides of the triangle ");
+    scanf("%d %d %d", &a, &b, &c);  
+    if(a==b && b==c){
+        printf("the triangle is equilateral \n");
+    }else if(a==b || b==c || a==c){
+        printf("the triangle is isosceles \n");
+    }else{
+        printf("the triangle is scalene \n");
+    }   
+    return 0;
+}
